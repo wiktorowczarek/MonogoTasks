7302ab04f9b0e4134036a2cb933fd9d8fd8f5fc2
Wiktor <wiktor.owczarek245@gmail.com>
2024-11-24 21:58:19 +0100

    Task one setup + helpers, constants

--- tests/elements.ts
+++ tests/elements.ts
@@ -1,8 +1,24 @@
 import { Page } from '@playwright/test';
+import { EnglishDomain, PolishDomain } from './constants';
 
-export function getLoginPageElements(page: Page) {
+export function getProductPageElements(page: Page, domain: string) {
   return {
-    loginTitle: page.locator('xpath=//'),
-    loginInput: page.locator('#\\:r2\\:'),
+    shopButton: page.locator(`[data-testid="headerItem-${domain ==  PolishDomain ? '1' : '0'}"]`),
+    shopMenuIndicator: page.locator('[data-page-name="shop"]'),
+    productBySKU: (sku: string) => page.locator(`[data-sku="${sku}"]`),
+    addToCartButton: page.locator('[data-testid="pdpAddToProduct"]'),
+    basketCount: page.locator('.IconLabeled-module-label-fmiDL.mini-cart__icon-label'),
+    openBasket: page.locator('[data-testid="cart"]'),
+    basketMenu: page.locator('.mini-cart mini-cart--open mini-cart--with-items'),
+  };
+}
+
+export function getBasketPageElements(page: Page) {
+  return {
+    basketContainer: page.locator('[data-testid="mini-cart-header"]'),
+    itemList: page.locator('[data-testid="mini-cart-list"]'),
+    getItemTitle: page.locator('.ProductMiniature-module-productName-cu7Vu'),
+    getItemPrice: page.locator('[data-testid="mini-cart-list"] .FormattedPrice-module-price-eDdEl'),
+    checkoutButton: page.locator('[data-testid="miniCartCheckoutButton"]'),
   };
 }
